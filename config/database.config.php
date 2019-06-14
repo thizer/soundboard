9d8e6f6722e9890727c0ae77ea8b6c75093dba98
Pernalonga <marco.a.braghim@gmail.com>
2019-06-14 20:41:03 -0300

    Framework suitup

---
+++ config/database.config.php
@@ -0,0 +1,8 @@
+<?php
+// return array(
+//   'host' => 'localhost',
+//   'database' => 'mysql',
+//   'username' => 'root',
+//   'password' => '',
+// );
+
---
+++ modules/ModuleDefault/Controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace ModuleDefault\Controllers;
+
+class IndexController extends AbstractController
+{
+  public function indexAction() {
+    // Here is where the legends begins...
+  }
+}
+
