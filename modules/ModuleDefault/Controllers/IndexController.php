aedba601f77a6d4686e2ad28f890585736d3ccce
Pernalonga <marco.a.braghim@gmail.com>
2019-06-14 22:12:25 -0300

    play some songs

--- modules/ModuleDefault/Controllers/IndexController.php
+++ modules/ModuleDefault/Controllers/IndexController.php
@@ -4,7 +4,29 @@ namespace ModuleDefault\Controllers;
 class IndexController extends AbstractController
 {
   public function indexAction() {
-    // Here is where the legends begins...
+    
+    $soundsPath = 'assets/sounds/';
+    $sounds = array();
+
+    if (is_dir($soundsPath)) {
+
+      foreach (scandir($soundsPath) as $item) {
+        if (is_dir("$soundsPath/$item") && !in_array($item, array('.', '..'))) {
+
+          foreach (scandir("$soundsPath/$item") as $file) {
+
+            if (is_file("$soundsPath/$item/$file")) {
+              $sounds[ucwords($item)][] = "$item/$file";
+            }
+          }
+        }
+      }
+
+    }
+
+    $this->addViewVar('sounds', $sounds);
+    $this->addViewVar('soundsPath', $soundsPath);
+
   }
 }
 
